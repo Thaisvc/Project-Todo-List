9348e3f801277e7d30218cc116536e0fb2d3ed16
thais <thaisviana081@outlook.com>
2022-04-25 20:40:19 -0300

    selecionado marcado

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 const selectInput = document.getElementById('texto-tarefa');
 const selectOl = document.getElementById('lista-tarefas');
 const selectButton = document.getElementById('criar-tarefa');
-/* document.querySelectorAll('.list')[0]; */
+/* document.querySelector('.list');  */
 //======================================================================================================================
 
 // adicionar tarefa a lista
@@ -17,38 +17,31 @@ selectButton.addEventListener('click', function () {//ADICIONANDO EVENTO AO BUTT
     criaLi.classList.add('list');
     //ADICIONANDO MINHA LI COMO FILHA DE OL NO MEU HTML
     selectOl.appendChild(criaLi);
+    //SE MINHA LI TIVER SIDO CRIADA
+    if( selectOl.appendChild(criaLi)){
+        //ADICIONO NOVA CLASS
+        document.querySelector('.list').classList.add('selected')
+            seleciona = document.getElementsByClassName('list');
+            function remove(){
+            for (let index = 0; index < seleciona.length; index += 1) {
+              let posicao = seleciona[index];
+            posicao.addEventListener('click', function(event) {
+                document.querySelector('.selected').classList.remove('selected');
+                event.target.classList.add('selected');
+              });
+            }
+           
+          }
+          remove();
+    }
     //LIMPANDO MEU INPUT PARA RECEBER NOVO VALOR
     selectInput.value = '';
 })
 //======================================================================================================================
 //mudando a cor de cada li clicada
-let selectLi = document.addEventListener('click', function (event) {
-// SE EVENT NO ELEMETO CLICADO (target) CONTER A CLASS LIST
-    if (event.target.classList.contains('list')) {
-        //REMOVE CLASS LIST E SELECTED 
-        removeClass('list', 'selected');
-        //EVENT NO ELEMETO CLICADO ADICIONA A CLASS SELECTED
-        event.target.classList.add('selected');
-        //SELECIONA MINHA LI PELA CLASS LIST
-        let classItem = document.getElementsByClassName('list');
-        //PERCORRO ELA
-        for (i = 0; i < classItem.length; i += 1) {
-            //ATRIBUI BACKGROUD BRANCO PARA CADA POSSIÇAO
-            classItem[i].style.backgroundColor = '';
-        }
-        //SELECIONA A CLASS SELECTED E ADICIONA A COR 
-        document.getElementsByClassName('selected')[0].style.backgroundColor = 'gray';
-    };
-});
-//remove a class
-function removeClass(nameClass, nameClassRemove) {
-//SELECIONADO CLASS SELECTED
-    let className = document.getElementsByClassName(nameClass);
-// PERCORRO ELA E REMOVO O CLASS SELECTED
-    for (i = 0; i < className.length; i += 1) {
-        className[i].classList.remove(nameClassRemove);
-    };
-};
+
+
+ 
 
 
 
