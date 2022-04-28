c2d51bb22ffef0ea1d2d602002993798a12d6621
thais <thaisviana081@outlook.com>
2022-04-28 17:11:08 -0300

    requisito 8

--- script.js
+++ script.js
@@ -7,41 +7,41 @@ const selectButton = document.getElementById('criar-tarefa');
 
 // adicionar tarefa a lista
 selectButton.addEventListener('click', function () {//ADICIONANDO EVENTO AO BUTTON COM UMA FUNÇAO
-    // PEGANDO O VALOR PASSADO PARA O INPUT
-    let valorInput = selectInput.value;
-    //CRIANDO MEU ELEMENTO LI FILHO DE OL
-    let criaLi = document.createElement('li');
-    //PASSANDO O TEXTO DO INPUT PARA MINHA LI |innerText = PARA MOSTRA O TEXTO PASSADO
-    criaLi.innerText = valorInput;
-    //ADICIONANDO UMA CLASS A MINHAS LI
-    criaLi.classList.add('list');
-    //ADICIONANDO MINHA LI COMO FILHA DE OL NO MEU HTML
-    selectOl.appendChild(criaLi);
-    //SE MINHA LI TIVER SIDO CRIADA
-    if( selectOl.appendChild(criaLi)){
-        //ADICIONO NOVA CLASS
-        document.querySelector('.list').classList.add('selected')
-            seleciona = document.getElementsByClassName('list');
-            function remove(){
-            for (let index = 0; index < seleciona.length; index += 1) {
-              let posicao = seleciona[index];
-            posicao.addEventListener('click', function(event) {
-                document.querySelector('.selected').classList.remove('selected');
-                event.target.classList.add('selected');
-              });
-            }
-           
-          }
-          remove();
+  // PEGANDO O VALOR PASSADO PARA O INPUT
+  let valorInput = selectInput.value;
+  //CRIANDO MEU ELEMENTO LI FILHO DE OL
+  let criaLi = document.createElement('li');
+  //PASSANDO O TEXTO DO INPUT PARA MINHA LI |innerText = PARA MOSTRA O TEXTO PASSADO
+  criaLi.innerText = valorInput;
+  //ADICIONANDO UMA CLASS A MINHAS LI
+  criaLi.classList.add('list');
+  //ADICIONANDO MINHA LI COMO FILHA DE OL NO MEU HTML
+  selectOl.appendChild(criaLi);
+  //SE MINHA LI TIVER SIDO CRIADA
+  if (selectOl.appendChild(criaLi)) {
+    //ADICIONO NOVA CLASS
+    document.querySelector('.list').classList.add('selected')
+    seleciona = document.getElementsByClassName('list');
+    function remove() {
+      for (let index = 0; index < seleciona.length; index += 1) {
+        let posicao = seleciona[index];
+        posicao.addEventListener('click', function (event) {
+          document.querySelector('.selected').classList.remove('selected', 'cor');
+          event.target.classList.add('selected', 'cor');
+
+        });
+      }
     }
-    //LIMPANDO MEU INPUT PARA RECEBER NOVO VALOR
-    selectInput.value = '';
+    remove();
+  }
+  //LIMPANDO MEU INPUT PARA RECEBER NOVO VALOR
+  selectInput.value = '';
 })
 //======================================================================================================================
 //mudando a cor de cada li clicada
 
 
- 
+
 
 
 
