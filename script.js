bf199d9bb451324b52ad2f200d23aada4a0429c2
thais <thaisviana081@outlook.com>
2022-04-29 10:32:02 -0300

    adicionado class completed

--- script.js
+++ script.js
@@ -2,11 +2,13 @@
 const selectInput = document.getElementById('texto-tarefa');
 const selectOl = document.getElementById('lista-tarefas');
 const selectButton = document.getElementById('criar-tarefa');
-/* document.querySelector('.list');  */
+const selectLi = document.getElementById('lista-tarefas').children;
+/* const selectCompleted = document.getElementsByClassName('completed'); */
 //======================================================================================================================
 
 // adicionar tarefa a lista
-selectButton.addEventListener('click', function () {//ADICIONANDO EVENTO AO BUTTON COM UMA FUNÇAO
+
+selectButton.addEventListener('click', function creates() {//ADICIONANDO EVENTO AO BUTTON COM UMA FUNÇAO
   // PEGANDO O VALOR PASSADO PARA O INPUT
   let valorInput = selectInput.value;
   //CRIANDO MEU ELEMENTO LI FILHO DE OL
@@ -18,48 +20,49 @@ selectButton.addEventListener('click', function () {//ADICIONANDO EVENTO AO BUTT
   //ADICIONANDO MINHA LI COMO FILHA DE OL NO MEU HTML
   selectOl.appendChild(criaLi);
   //SELECIONANDO MINHA PRIMEIRA CALSS LIST E ADICIONANDO A CLASS SELECTED
-    document.querySelector('.list').classList.add('selected')
-    //SELECIONANDO TODAS AS CLASS LIST
-    seleciona = document.getElementsByClassName('list');
-    //FUNÇAO PARA SDD E REMOVER A CLASS SELECTED E A CLASS COR
-    function remove() {
-      //PERCORRENDO TODAS AS CLASS LIST
-      for (let index = 0; index < seleciona.length; index += 1) {
-        //GUARDANDO A POSICAO
-        let posicao = seleciona[index];
-        //ADICIONANDO UM ESCUTADOR DE EVENTO A ADA POSICAO
-        posicao.addEventListener('click', function (event) {
-          //SELECIONANDO MINHA CLASS SELECTED/REMOVENDO A CLASS SELECTED E COR COSA TIVER /A CADA CLICK
-          document.querySelector('.selected').classList.remove('selected', 'cor');
-          //ADICIONADO CLASS SELECTED E COR SE NAO TIVER/A CADA CLICK
-          event.target.classList.add('selected', 'cor');
+  document.querySelector('.list').classList.add('selected')
+  //SELECIONANDO TODAS AS CLASS LIST
+  seleciona = document.getElementsByClassName('list');
+  //FUNÇAO PARA SDD E REMOVER A CLASS SELECTED E A CLASS COR
+  function remove() {
+    //PERCORRENDO TODAS AS CLASS LIST
+    for (let index = 0; index < seleciona.length; index += 1) {
+      //GUARDANDO A POSICAO
+      let posicao = seleciona[index];
+      //ADICIONANDO UM ESCUTADOR DE EVENTO A ADA POSICAO
+      posicao.addEventListener('click', function (event) {
+        //SELECIONANDO MINHA CLASS SELECTED/REMOVENDO A CLASS SELECTED E COR COSA TIVER /A CADA CLICK
+        document.querySelector('.selected').classList.remove('selected', 'cor');
+        //ADICIONADO CLASS SELECTED E COR SE NAO TIVER/A CADA CLICK
+        event.target.classList.add('selected', 'cor');
 
-        });
-      }
+      });
     }
-    remove();
-  
+  }
+  remove();
+
   //LIMPANDO MEU INPUT PARA RECEBER NOVO VALOR
   selectInput.value = '';
+  for (let index = 0; index < selectLi.length; index += 1) {// aqui eu adicionei o evento de duplo click nas li's que seriam criadas
+    selectLi[index].addEventListener("dblclick", (event) => {
+      event.target.classList.replace('list', 'completed')
+    })
+    
+  
+  }
+  
 })
 //======================================================================================================================
-//mudando a cor de cada li clicada
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+/* for (let index = 0; index < selectLi.length; index += 1) {
+  selectLi[index].addEventListener("dblclick", () => {
+    console.log(selectLi) 
+    alert('click')
+  })
+}
+ */
 
 
 
